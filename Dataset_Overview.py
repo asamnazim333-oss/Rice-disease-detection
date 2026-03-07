d4a00944dba53ef8f4b5a22a82b9eee18500c37b
asamnazim333-oss <asamnazim333@gmail.com>
2026-03-07 14:26:45 +0500

    Create Dataset_Overview.py

---
+++ Dataset_Overview.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("📊 Dataset Overview")
+
+st.write("""
+Dataset contains rice leaf images categorized into diseases such as:
+- Healthy
+- Bacterial Blight
+- Blast
+- Brown Spot
+
+Total images: (mention your dataset size)
+Image resolution: (mention size)
+Source: your dataset
+""")
