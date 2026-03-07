000e4c9ed355d1ec8333233814919b49ebc631ef
asamnazim333-oss <asamnazim333@gmail.com>
2026-03-07 14:28:49 +0500

    Create Model_Evaluation.py

---
+++ Model_Evaluation.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("📈 Model Evaluation")
+
+st.write("""
+Evaluation metrics (example):
+- Accuracy: 85%
+- Precision: 83%
+- Recall: 80%
+- F1 Score: 82%
+
+You can later add real evaluation results.
+""")
