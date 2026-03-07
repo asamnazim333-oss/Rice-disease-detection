41a6b2a80578231e2561621b7de6a25c1a62318e
asamnazim333-oss <asamnazim333@gmail.com>
2026-03-07 17:06:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 
-st.set_page_config(page_title="🌾 Rice Disease App", layout="wide")
+st.set_page_config(
+    page_title="🌾 Rice Disease Detection",
+    layout="wide"
+)
 
 st.title("🌾 Rice Disease Detection App")
-st.write("Use sidebar to navigate pages.")
 
-st.sidebar.success("Select a page above.")
 st.markdown("""
-<style>
-    .main {
-        background-color: #f5f7ff;
-    }
-    h1 {
-        color: #2c3e50;
-    }
-</style>
-""", unsafe_allow_html=True)
+Use the left sidebar to navigate between pages:
+- Dataset Overview
+- Preprocessing
+- Classification
+- Model Evaluation
+""")
